f634032898741c3c6976963386edfcf2f6155d1e
Brittany Ridley <britt.ridley@outlook.com>
2021-09-16 16:09:59 -0500

    Add project files.

---
+++ Variables_Exercise2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Variables_Exercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
